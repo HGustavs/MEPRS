d03bf19c954052376aac66cdfe0229b4a7c85ee1
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2015-10-29 12:05:47 +0100

    Added support for score font graphics.

--- src/feature_draw_bitmap.c
+++ src/feature_draw_bitmap.c
@@ -4,13 +4,16 @@
 static Window *s_main_window;
 static Layer *s_image_layer;
 static GBitmap *s_image;
-static GBitmap *t_image[25];
+static GBitmap *t_image[64];
 
 static void layer_update_callback(Layer *layer, GContext* ctx) {
-  graphics_draw_bitmap_in_rect(ctx, t_image[6], GRect(30, 30, 12, 12));
+  graphics_draw_bitmap_in_rect(ctx, t_image[13], GRect(30, 30, 12, 12));
   graphics_draw_bitmap_in_rect(ctx, t_image[7], GRect(42, 30, 12, 12));
-  graphics_draw_bitmap_in_rect(ctx, t_image[8], GRect(54, 30, 12, 12));
+  graphics_draw_bitmap_in_rect(ctx, t_image[15], GRect(54, 30, 12, 12));
   graphics_draw_bitmap_in_rect(ctx, t_image[3], GRect(66, 30, 12, 12));
+
+  graphics_draw_bitmap_in_rect(ctx, t_image[25], GRect(100, 100, 14, 21));
+
 }
 
 static void main_window_load(Window *window) {
@@ -27,15 +30,14 @@ static void main_window_load(Window *window) {
 	// Define tiles (2 rows of 12 tiles at 12x12 px)
 	for(int i=0;i<12;i++){
 			t_image[i] = gbitmap_create_as_sub_bitmap(s_image, GRect(i*12, 0, 12, 12));			
+			t_image[i+12] = gbitmap_create_as_sub_bitmap(s_image, GRect(i*12, 12, 12, 12));			
+			if(i<10) t_image[i+24] = gbitmap_create_as_sub_bitmap(s_image, GRect(i*14, 24, 14, 21));
 	}	
-	for(int i=12;i<24;i++){
-			t_image[i] = gbitmap_create_as_sub_bitmap(s_image, GRect((i-12)*12, 0, 12, 12));			
-	}		
 }
 
 static void main_window_unload(Window *window) {
 	// Deallocate tiles
-	for(int i=0;i<24;i++){
+	for(int i=0;i<34;i++){
 			 gbitmap_destroy(t_image[i]);
 	}
 	
