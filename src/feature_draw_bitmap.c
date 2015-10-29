d7557e71a03fae4208406d01a6d8d908a5d46bf8
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2015-10-29 11:10:22 +0100

    Automatic Tile Allocation

--- src/feature_draw_bitmap.c
+++ src/feature_draw_bitmap.c
@@ -4,7 +4,7 @@
 static Window *s_main_window;
 static Layer *s_image_layer;
 static GBitmap *s_image;
-static GBitmap *t_image[4];
+static GBitmap *t_image[20];
 
 static void layer_update_callback(Layer *layer, GContext* ctx) {
   graphics_draw_bitmap_in_rect(ctx, t_image[0], GRect(30, 30, 12, 12));
@@ -21,22 +21,25 @@ static void main_window_load(Window *window) {
   layer_set_update_proc(s_image_layer, layer_update_callback);
   layer_add_child(window_layer, s_image_layer);
 
+	// Define bitmap
   s_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NO_LITTER);
 
 	// Define tiles
-	t_image[0] = gbitmap_create_as_sub_bitmap(s_image, GRect(0, 0, 12, 12));
-	t_image[1] = gbitmap_create_as_sub_bitmap(s_image, GRect(12, 0, 12, 12));
-	t_image[2] = gbitmap_create_as_sub_bitmap(s_image, GRect(24, 0, 12, 12));
-	t_image[3] = gbitmap_create_as_sub_bitmap(s_image, GRect(36, 0, 12, 12));
-	
+	for(int i=0;i<10;i++){
+			t_image[i] = gbitmap_create_as_sub_bitmap(s_image, GRect(t*12, 0, 12, 12));			
+	}	
 }
 
 static void main_window_unload(Window *window) {
-  gbitmap_destroy(s_image);
-  gbitmap_destroy(t_image[0]);
-  gbitmap_destroy(t_image[1]);
-  gbitmap_destroy(t_image[2]);
-  gbitmap_destroy(t_image[3]);
+	// Deallocate tiles
+	for(int i=0;i<10;i++){
+			 gbitmap_destroy(t_image[i]);
+	}
+	
+	// Deallocate bitmap
+ 	gbitmap_destroy(s_image);
+	
+	// Destroy layer
 	layer_destroy(s_image_layer);
 }
 
